01f03f7f314505a28de8537dc027737078963365
Muhammad Taha <smuhammadtaha19@gmail.com>
2020-02-20 17:36:29 +0500

    intial-commit

---
+++ app/src/main/java/com/example/muhammadtaha/top10downloader/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.muhammadtaha.top10downloader
+
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.net.URL
+
+class FeedEntry{
+    var name:String = ""
+    var artist : String = ""
+    var releaseDate : String = ""
+    var summary : String = ""
+    var imageURL : String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+            """.trimIndent()
+    }
+}
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        Log.d(TAG,"onCreate:done")
+    }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            Log.d(TAG,"onPostExecute: parameter is $result")
+
+        }
+
+        override fun doInBackground(vararg url: String?): String {
+            Log.d(TAG, "doInBackground: starts with ${url[0]}")
+            val rssFeed = downloadXML(url[0])
+            if(rssFeed.isEmpty())
+            {
+                Log.e(TAG, "doInBackground: Error downloading")
+            }
+            return rssFeed
+        }
+
+            private fun downloadXML(urlPath: String?):String{
+                return URL(urlPath).readText()
+            }
+
+        }
+    }
+}
